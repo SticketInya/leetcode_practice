bbb3055cadd9985c03cf7f946dbf45b955b72351
JPatrik <jokhel.roland@gmail.com>
2025-01-11 13:02:38 +0100

    feat: add bucket sort solution to problem 347

--- 347_top_k_frequent_elements/solution.py
+++ 347_top_k_frequent_elements/solution.py
@@ -8,12 +8,27 @@ class Solution:
         for num in nums:
             frequency_map[num] += 1
 
-        print(frequency_map)
-        print(sorted(frequency_map))
-        print(sorted(frequency_map.items(), key=lambda x: x[1]))
         return [
             key
             for key, _ in sorted(
                 frequency_map.items(), key=lambda x: x[1], reverse=True
             )[:k]
         ]
+
+    def top_k_with_bucket_sort(self, nums: List[int], k: int) -> List[int]:
+        frequency_map: Dict[int, int] = defaultdict(int)
+        for num in nums:
+            frequency_map[num] += 1
+
+        frequencies: List[List[int]] = [[] for _ in range(len(nums) + 1)]
+        for num, count in frequency_map.items():
+            frequencies[count].append(num)
+
+        top_k: List[int] = []
+        for i in range(len(frequencies) - 1, 0, -1):
+            for num in frequencies[i]:
+                top_k.append(num)
+                if len(top_k) == k:
+                    return top_k
+
+        return []
--- 347_top_k_frequent_elements/test.py
+++ 347_top_k_frequent_elements/test.py
@@ -5,7 +5,7 @@ from solution import Solution
 
 class TestTopKFrequentElements:
 
-    def test_group_anagrams(self) -> None:
+    def test_top_frequent_k(self) -> None:
         tests: List[Tuple[List[int], int, List[int]]] = [
             ([1, 1, 1, 2, 2, 3], 2, [1, 2]),
             ([1], 1, [1]),
@@ -14,6 +14,20 @@ class TestTopKFrequentElements:
 
         for nums, k, output in tests:
             got = Solution().top_frequent_k(nums, k)
+            if sorted(got) != sorted(output):
+                pytest.fail(
+                    f"Invalid result received for ({nums}, {k}); want={output}; got={got}!"
+                )
+
+    def test_top_k_with_bucket_sort(self) -> None:
+        tests: List[Tuple[List[int], int, List[int]]] = [
+            ([1, 1, 1, 2, 2, 3], 2, [1, 2]),
+            ([1], 1, [1]),
+            ([4, 1, -1, 2, -1, 2, 3], 2, [2, -1]),
+        ]
+
+        for nums, k, output in tests:
+            got = Solution().top_k_with_bucket_sort(nums, k)
             if sorted(got) != sorted(output):
                 pytest.fail(
                     f"Invalid result received for ({nums}, {k}); want={output}; got={got}!"
