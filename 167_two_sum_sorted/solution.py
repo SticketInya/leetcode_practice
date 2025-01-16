3de993292e68f34cf1da949dc7efaaf76fdb25a9
JPatrik <patrik.jokhel@dakai.io>
2025-01-16 13:59:49 +0100

    fix: fix incorrect space complexity for  problem 167

--- 167_two_sum_sorted/solution.py
+++ 167_two_sum_sorted/solution.py
@@ -4,20 +4,13 @@ from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
-        unique_nums = set(numbers)
 
         while left < right:
-            if target - numbers[left] not in unique_nums:
-                left += 1
-                continue
-
-            while left < right and numbers[left] + numbers[right] != target:
+            if numbers[left] + numbers[right] > target:
                 right -= 1
-
-            if numbers[left] + numbers[right] == target:
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
                 return [left + 1, right + 1]
-
-            left += 1
-            right = len(numbers) - 1
 
         return [0, 0]
